6d2f0b5f6613a0c832d76c4eaa094b1c26ccb5f0
Nate Michael <nate.michael@gmail.com>
2011-09-08 13:15:47 -0400

    Adding license and credits. Releasing under GPL v3

--- src/DigimeshBase.cc
+++ src/DigimeshBase.cc
@@ -1,5 +1,22 @@
-// Implementation of base inheritance class, provides I/O functionality
-// N. Michael, UPenn 9/11
+/*
+  This file is part of digimesh, an interface to
+  use the digimesh functionality available via Digi.
+
+  digimesh is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Nathan Michael, Sept. 2011
+*/
 
 #include <digimesh/DigimeshBase.h>
 #include <iostream>
